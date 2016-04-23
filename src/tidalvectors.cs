0e1b5e27ec38b86bc7d0b7163147b5cf81d72006
Marcus Rugger <marcusrugger@yahoo.com>
2016-04-23 11:03:20 -0400

    Clean up

--- src/tidalvectors.cs
+++ src/tidalvectors.cs
@@ -1,77 +1,43 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 
 class TidalVectors
 {
-    public const int SLICE_DEGREES = 10;
-    public const int SLICE_COUNT   = 360 / SLICE_DEGREES;
-    public Vector[] vectors;
+    private static int SLICE_DEGREES = 15;
+    private static int SLICE_COUNT   = 360 / SLICE_DEGREES;
 
-    static public double toRadians(double degrees)
+    private static GravitationalForce gforce  = new GravitationalForce(Constants.Moon.MASS);
+    private static Cartesian positionMoon     = new Cartesian(Constants.Moon.MEAN_DISTANCE, 0.0);
+    private static Cartesian forceEarthCenter = new Cartesian(gforce.compute(Constants.Moon.MEAN_DISTANCE), 0.0);
+
+    private static double toRadians(double degrees)
     {
         return Math.PI * degrees / 180.0;
     }
 
-    static public double toDegrees(double radians)
+    private static double toDegrees(double radians)
     {
         return 180.0 * radians / Math.PI;
     }
 
-    public TidalVectors()
+    public static IEnumerable<Vector> Create()
     {
-        GravitationalForce gforce  = new GravitationalForce(Constants.Moon.MASS);
-        Cartesian positionMoon     = new Cartesian(Constants.Moon.MEAN_DISTANCE, 0.0);
-        Cartesian forceEarthCenter = new Cartesian(gforce.compute(Constants.Moon.MEAN_DISTANCE), 0.0);
-
         // Create a uniform list of angles from 0 - 360 degrees
-        double[] angles = Enumerable.Range(0, 36)
-                                    .Select(n => toRadians(10.0 * (double) n))
-                                    .ToArray();
+        IEnumerable<double> angles = Enumerable.Range(0, SLICE_COUNT)
+                                               .Select(n => toRadians(SLICE_DEGREES * (double) n));
 
         // Tranform angles into points on a circle of Earth radius
-        Cartesian[] points2d = angles.Select(a => new Polar(a, Constants.Earth.MEAN_RADIUS).ToCartesian())
-                                     .ToArray();
+        IEnumerable<Cartesian> points2d = angles.Select(a => new Polar(a, Constants.Earth.MEAN_RADIUS).ToCartesian());
 
         // Calculate lunar gravitational force at each point
-        Cartesian[] force2d = points2d.Select(p => new Polar(positionMoon - p).TransformR(gforce.compute).ToCartesian())
-                                      .ToArray();
+        IEnumerable<Cartesian> force2d = points2d.Select(p => new Polar(positionMoon - p).TransformR(gforce.compute).ToCartesian());
 
         // Subtract gravitational force at Earth's center and convert to micronewtons
-        Cartesian[] relative2d = force2d.Select(f => (f - forceEarthCenter).Multiply(1e6))
-                                        .ToArray();
+        IEnumerable<Cartesian> relative2d = force2d.Select(f => (f - forceEarthCenter).Multiply(1e6));
 
         // Combine points and forces into vector
-        vectors = points2d.Zip(relative2d, (p, f) => new Vector(p, f))
-                          .ToArray();
-    }
-
-    public void Draw(Graphics graphics)
-    {
-        foreach (var vector in vectors)
-        {
-            var pts = TransformToDisplayPoints(vector);
-            Draw(graphics, pts.Item1, pts.Item2);
-        }
-    }
-
-    private Tuple<Point, Point> TransformToDisplayPoints(Vector vector)
-    {
-        const double DISPLAY_RADIUS = 300;
-
-        var v = vector.p.Multiply(DISPLAY_RADIUS / Constants.Earth.MEAN_RADIUS);
-        var p1 = new Point(500 + (int) v.x, 500 + (int) v.y);
-        var p2 = new Point((int) (50.0 * vector.f.x), (int) (50.0 * vector.f.y));
-
-        p2.Offset(p1);
-
-        return Tuple.Create(p1, p2);
-    }
-
-    private void Draw(Graphics graphics, Point p1, Point p2)
-    {
-        graphics.DrawLine(Pens.Red, p1, p2);
-        graphics.FillEllipse(Brushes.Green, p1.X - 2, p1.Y - 2, 5, 5);
-        graphics.FillEllipse(Brushes.Blue , p2.X - 2, p2.Y - 2, 5, 5);
+        return points2d.Zip(relative2d, (p, f) => new Vector(p, f));
     }
 }
--- src/tides.cs
+++ src/tides.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 public class Tides : Form
 {
-    private TidalVectors vectors = new TidalVectors();
+    private IEnumerable<Tuple<Point, Point>> segments;
 
     static public void Main ()
     {
@@ -15,6 +17,23 @@ public class Tides : Form
     {
         Text = "Tides";
         Size = new System.Drawing.Size(1000, 1000);
+
+        IEnumerable<Vector> vectors = TidalVectors.Create();
+        segments = vectors.Select(TransformToDisplayPoints);
+    }
+
+    private Tuple<Point, Point> TransformToDisplayPoints(Vector vector)
+    {
+        const double DISPLAY_RADIUS = 300;
+        var v = vector.p.Multiply(DISPLAY_RADIUS / Constants.Earth.MEAN_RADIUS);
+
+        var p1 = new Point((int) (v.x + 0.5), (int) (v.y + 0.5));
+        p1.Offset(500, 500);
+
+        var p2 = new Point((int) (50.0 * vector.f.x + 0.5), (int) (50.0 * vector.f.y + 0.5));
+        p2.Offset(p1);
+
+        return Tuple.Create(p1, p2);
     }
 
     protected override void OnPaint(PaintEventArgs e)
@@ -26,12 +45,20 @@ public class Tides : Form
     private void DrawIt(Graphics graphics)
     {
         DrawEarth(graphics);
-        vectors.Draw(graphics);
+        foreach (var pair in segments)
+            DrawSegment(graphics, pair.Item1, pair.Item2);
     }
 
     private void DrawEarth(Graphics graphics)
     {
         Rectangle rectangle = new Rectangle(200, 200, 600, 600);
         graphics.FillEllipse(System.Drawing.Brushes.Aqua, rectangle);
+    }
+
+    private void DrawSegment(Graphics graphics, Point p1, Point p2)
+    {
+        graphics.DrawLine(Pens.Red, p1, p2);
+        graphics.FillEllipse(Brushes.Green, p1.X - 2, p1.Y - 2, 5, 5);
+        graphics.FillEllipse(Brushes.Blue , p2.X - 2, p2.Y - 2, 5, 5);
     }
 }
